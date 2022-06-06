0683972951eb581e17fcdc2805b60b41bc69cdfc
vikash1a <vikash1a2b3c@gmail.com>
2022-06-07 00:37:55 +0530

    double linked list, graph cycle detection

--- Data Structure/Graph/Graph.cpp
+++ Data Structure/Graph/Graph.cpp
@@ -11,9 +11,20 @@ class Graph{
             //
 
             //dfs
-            vector<vector<int>> adj = createGraph();
-            vector<int> dfsV = bfs(adj);
-            print(dfsV);
+            // vector<vector<int>> adj = createGraph();
+            // vector<int> dfsV = bfs(adj);
+            // print(dfsV);
+            //
+
+            //detect cycle in undirected graph
+            vector<vector<int>> vvg ;
+            vvg.push_back({1,2});
+            vvg.push_back({0,2});
+            vvg.push_back({0,1,3});
+            vvg.push_back({2});
+
+            bool result = detectCycle(vvg);
+            cout<<" cycle detected - "<<result<<endl;
             //
             return ;
         }
@@ -73,6 +84,37 @@ class Graph{
                 }
             }
             return;
+        }
+        //
+
+        //detect cycle in undirected graph
+        bool detectCycle(vector<vector<int>> adj){
+            int na = adj.size()-1;
+            vector<bool> visited(na+1,false);
+            for(int i=0;i<=na;i++){
+                if(!visited[i]){
+                    visited[i]=true;
+                    if(detectCycleUtil(adj,i,-1,visited))return true;
+                    visited[i]=false;
+                    // cout<<i<<"-"<<endl;
+                }
+                
+            }
+            return false;
+        }
+        bool detectCycleUtil(vector<vector<int>> adj, int k,int parent, vector<bool> visited){
+            
+            for(int i=0;i<=adj[k].size()-1;i++){
+                // cout<<parent<<"_"<<"_"<<k<<"_"<<i<<endl;
+                int m = adj[k][i];
+                if(!visited[m]){
+                    visited[m]=true;
+                    bool result = detectCycleUtil(adj,m,k,visited);
+                    if(result)return true;
+                }
+                else if(m != parent)return true;
+            }
+            return false;
         }
         //
 
--- Data Structure/LinkedList/LinkedList.cpp
+++ Data Structure/LinkedList/LinkedList.cpp
@@ -1,6 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class DoubleNode{
+    public:
+        int val;
+        DoubleNode* next;
+        DoubleNode* prev;
+        DoubleNode(int x){
+            val = x;
+        }
+        DoubleNode* addStart(DoubleNode* node, int x){
+            DoubleNode* newNode = new DoubleNode(x);
+            newNode->next = node;
+            node->prev = newNode;
+            return newNode;
+        }
+        DoubleNode* addEnd(DoubleNode* node, int x){
+            DoubleNode* newNode = new DoubleNode(x);
+            DoubleNode* ans = node;
+            while(node->next!=NULL){
+                node = node->next;
+            }
+            node->next = newNode;
+            newNode->prev = node;
+            return ans;
+        }
+        void print(DoubleNode* node){
+            while(node!=NULL){
+                cout<<node->val<<" ";
+                node=node->next;
+            }
+            cout<<endl;
+            return;
+        }
+};
+
 class Node
 {
     public:
@@ -43,12 +77,19 @@ int main()
 {
     cout<<"Hello World\n";
 
-    Node* node = new Node(0);
-    addNode(node,new Node(1));
-    addNode(node->next,2);
-    print(node);
-    deleteNode(node,node->next);
-    print(node);
+    // Node* node = new Node(0);
+    // addNode(node,new Node(1));
+    // addNode(node->next,2);
+    // print(node);
+    // deleteNode(node,node->next);
+    // print(node);
+
+    DoubleNode* doubleNode = new DoubleNode(1);
+    doubleNode = doubleNode->addStart(doubleNode,0);
+    doubleNode =doubleNode->addEnd(doubleNode,2);
+    doubleNode =doubleNode->addStart(doubleNode,-1);
+    doubleNode =doubleNode->addEnd(doubleNode,3);
+    doubleNode->print(doubleNode);
 
     return 0;
 }
