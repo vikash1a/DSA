e66013e55c38fdf71e29188921b271b4267af7e3
Vikash Sinha <vikash1a2b3c@gmail.com>
2024-08-04 06:37:21 +0000

    heap sort done

---
+++ v2/Algorithms/Sorting/HeapSort.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(vector<int> v){
+    for(int k: v){
+        cout<<k<<" ";
+    }
+    cout<<endl;
+}
+void heapifyHelper(vector<int>& v, int i, int n){
+    int l = 2*i+1, r = 2*i+2;
+    if(r>n)return;
+    if(v[i]<v[l] && v[l]>v[r]){
+        swap(v[i],v[l]);
+        heapifyHelper(v,l,n);
+    }
+    else if(v[i]<v[r]){
+        swap(v[i],v[r]);
+        heapifyHelper(v,r,n);
+    }
+    return;
+}
+void heapify(vector<int>& v){
+    int n = v.size()-1;
+    for(int i=n/2-1; i>=0; i--){
+        heapifyHelper(v,i,n);
+        // print(v);
+    }
+}
+void heapSort(vector<int> &v){
+    int n = v.size()-1;
+    heapify(v);
+    for(int i=n;i>=0;i--){
+        swap(v[0],v[i]);
+        heapifyHelper(v,0,i-1);
+    }
+    return;
+}
+
+int main(){
+    cout<<"hello world"<<endl;
+    vector<int> v = {4,2,6,3,7,5,4,32,5};
+    heapSort(v);
+    print(v);
+    return 0;
+}
